247faa28329760b744863b50e6efb645dfce9744
FynnLaw <fynnlaw@gmail.com>
2016-07-07 16:05:26 +0800

    spring test

---
+++ src/main/java/com/fynn/Person.java
@@ -0,0 +1,38 @@
+package com.fynn;
+
+public class Person {
+	private String name;
+	private int age;
+
+	public Person(){
+		System.out.println("default constructor");
+	}
+	
+	public Person(String tel,int number){
+		System.out.println("parameters constructor");
+		System.out.println(tel);
+		System.out.println(number);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("method setName");
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("method setAge");
+		this.age = age;
+	}
+
+	public void init(){
+		System.out.println("init method");
+	}
+}
